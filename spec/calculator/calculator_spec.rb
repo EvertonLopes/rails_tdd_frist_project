7fa0bc61218250de6aafac8d5f4b0735b3d1a80b
Everton L. Costa <everton.lopess@outlook.com>
2019-03-22 17:25:16 -0300

    Criado três testes com: specify, example, it, para o método SUM.

--- spec/calculator/calculator_spec.rb
+++ spec/calculator/calculator_spec.rb
@@ -1,5 +1,22 @@
 require 'calculator'
 
-describe Calculator do
+RSpec.describe Calculator do
+  specify 'sum method for two numbers' do
+    calc = Calculator.new
+    result = calc.sum(5, 7)
+    expect(result).to eq(12)
+  end
+
+  example 'sum method for two numbers' do
+    calc = Calculator.new
+    result = calc.sum(5, 7)
+    expect(result).to eq(12)
+  end
+
+  it 'sum method for two numbers with number negative.' do
+    calc = Calculator.new
+    result = calc.sum(-5, 7)
+    expect(result).to eq(2)
+  end
 
 end
