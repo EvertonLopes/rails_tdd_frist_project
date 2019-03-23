c338133320117ee2591cb5f8d2ce7da840a94226
Everton L. Costa <everton.lopess@outlook.com>
2019-03-23 12:45:04 -0300

    Adicionamos comentários com exemplos de subject implícito e explícito.

--- spec/calculator/calculator_spec.rb
+++ spec/calculator/calculator_spec.rb
@@ -1,6 +1,6 @@
 require 'calculator'
 
-describe Calculator do
+describe Calculator, "- Teste de Matemática" do
   context '#use number integer: ' do
     specify 'method sum' do
       result = subject.sum(10, 7)
@@ -13,19 +13,33 @@ describe Calculator do
     end
 
     example 'method div' do
-      result = subject.div(15, 3)
-      expect(result).to eq(5)
+      expect(subject.div(15, 3)).to eq(5)
     end
 
-    it 'method mult' do
+    xit 'method mult' do
       result = subject.mult(3, 7)
       expect(result).to eq(21)
     end
-
-    it 'without content'
   end
 end
 
+# subject implícito
+#   result = subject.sum(x, y) # Não precisa instânciar a classe.
+
+# subject explícito
+#   subject(:variavel) { described_class.new() } # Demostro a chamada da classe.
+#   result = variavel.sum(x, y)
+
+# Before
+# context '#use number integer: ' do
+#   specify 'method sum' do
+#     cal = Calculator.new()
+#     result = calc.sum(10, 7) #Podemos trocar calc por subject
+#     expect(result).to eq(17)
+#   end
+# end
+
+# After
 # context '#use number integer: ' do
 #   specify 'method sum' do
 #     result = subject.sum(10, 7) #Podemos trocar calc por subject
