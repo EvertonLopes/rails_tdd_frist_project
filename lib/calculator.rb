c93ce1ba2ea35c0c158d46e905267c04584d566e
Everton L. Costa <everton.lopess@outlook.com>
2019-03-23 11:34:03 -0300

    Três métodos criados, sub, div, mult.

--- lib/calculator.rb
+++ lib/calculator.rb
@@ -2,4 +2,16 @@ class Calculator
   def sum(x, y)
     return x + y
   end
+
+  def sub(x, y)
+    return x - y
+  end
+
+  def div(x, y)
+    return x / y
+  end
+
+  def mult(x, y)
+    return x * y
+  end
 end
