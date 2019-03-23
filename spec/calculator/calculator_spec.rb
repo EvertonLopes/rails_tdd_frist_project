e0caf31f81615b9113ed5b0e1b8252a682486213
Everton L. Costa <everton.lopess@outlook.com>
2019-03-23 11:44:57 -0300

    Retiramos a instância da Classe e usamos o método subject.

--- spec/calculator/calculator_spec.rb
+++ spec/calculator/calculator_spec.rb
@@ -1,25 +1,35 @@
 require 'calculator'
 
 describe Calculator do
+
+  subject(:calc) { described_class.new() }
+
   context '#use number integer: ' do
     specify 'method sum' do
-      result = subject.sum(10, 7)
+      result = calc.sum(10, 7)
       expect(result).to eq(17)
     end
 
     specify 'method sub' do
-      result = subject.sub(7, 6)
+      result = calc.sub(7, 6)
       expect(result).to eq(1)
     end
 
     example 'method div' do
-      result = subject.div(15, 3)
+      result = calc.div(15, 3)
       expect(result).to eq(5)
     end
 
     it 'method mult' do
-      result = subject.mult(3, 7)
+      result = calc.mult(3, 7)
       expect(result).to eq(21)
     end
   end
 end
+
+# context '#use number integer: ' do
+#   specify 'method sum' do
+#     result = subject.sum(10, 7) #Podemos trocar calc por subject
+#     expect(result).to eq(17)
+#   end
+# end
