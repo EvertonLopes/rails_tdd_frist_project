49bbbbb6dcd8fe2e0aa5b4a0ed9c113fb5e484ea
Everton L. Costa <everton.lopess@outlook.com>
2019-03-23 11:58:40 -0300

    Arquivo calculator_spec: retirada linha subject e substiruído calc por subject.

--- spec/calculator/calculator_spec.rb
+++ spec/calculator/calculator_spec.rb
@@ -1,29 +1,28 @@
 require 'calculator'
 
 describe Calculator do
-
-  subject(:calc) { described_class.new() }
-
   context '#use number integer: ' do
     specify 'method sum' do
-      result = calc.sum(10, 7)
+      result = subject.sum(10, 7)
       expect(result).to eq(17)
     end
 
     specify 'method sub' do
-      result = calc.sub(7, 6)
+      result = subject.sub(7, 6)
       expect(result).to eq(1)
     end
 
     example 'method div' do
-      result = calc.div(15, 3)
+      result = subject.div(15, 3)
       expect(result).to eq(5)
     end
 
     it 'method mult' do
-      result = calc.mult(3, 7)
+      result = subject.mult(3, 7)
       expect(result).to eq(21)
     end
+
+    it 'without content'
   end
 end
 
