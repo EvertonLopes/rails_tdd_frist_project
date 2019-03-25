69b0ab95875ad274900cb5597a9f430f6b13d963
Everton L. Costa <everton.lopess@outlook.com>
2019-03-25 15:44:35 -0300

    Matchers_Predicate: teste para resultados com predicados.

---
+++ spec/matchers/predicate_spec.rb
@@ -0,0 +1,16 @@
+describe 'Predicados' do
+  it 'Ímpar' do
+    expect(3).to be_odd
+  end
+
+  it 'Par' do
+    expect(4).to be_even
+  end
+end
+
+
+
+
+#be_nil - nil?
+#be_zero - .zero?
+#be_empty - empty?
