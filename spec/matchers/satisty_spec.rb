36a5c41b2b6cc067e45ab20389d29872dd40b4a4
Everton L. Costa <everton.lopess@outlook.com>
2019-03-27 17:13:28 -0300

    Rubocop: refatoracao de codigo, melhoria.

--- spec/matchers/satisty_spec.rb
+++ spec/matchers/satisty_spec.rb
@@ -1,8 +1,10 @@
 describe 'Matcher Satisfy: ' do
-  it { expect(10).to satisfy { |x| x % 2 == 0 } }
-  it { 
+  it {
     expect(9).to satisfy('be a multiple of 3: ') do |x|
-       x % 3 == 0
+      (x % 3).zero?
     end
   }
+
+  # Valido, porem Rubocop nao aceita
+  # it { expect(10).to satisfy { |x| (x % 2).zero? } }
 end
